6f6d572e660f69508c7b5d5b60439d00ba6c3dd1
accaliamba061305 <accaliamba061305@gmail.com>
2026-06-30 21:29:52 +0700

    html

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const sidebar = document.querySelector(".sidebar");
-const mainContent = document.querySelector(".main-content");
+document.getElementById('language').addEventListener('change', function() {
+    const selectedLanguage = this.value;
+    
+    if (selectedLanguage === 'en') {
+        window.location.href = 'index-en.html'; // Go to English page
+    } else if (selectedLanguage === 'vi') {
+        window.location.href = 'index.html';    // Go to Vietnamese page
+    }
+});
