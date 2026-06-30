2cc46c6260408bb248353d93d9728b9924a2f205
accaliamba061305 <accaliamba061305@gmail.com>
2026-06-30 21:34:45 +0700

    s

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 document.getElementById('language').addEventListener('change', function() {
     const selectedLanguage = this.value;
     
+    // Get the current URL path (e.g., /repository-name/index.html)
+    let currentPath = window.location.pathname;
+    
     if (selectedLanguage === 'en') {
-        window.location.href = 'index-en.html'; // Go to English page
+        // If we are on the default page, replace it with the English file
+        if (currentPath.endsWith('/') || currentPath.endsWith('index.html')) {
+            window.location.href = 'index-en.html';
+        }
     } else if (selectedLanguage === 'vi') {
-        window.location.href = 'index.html';    // Go to Vietnamese page
+        if (currentPath.endsWith('index-en.html')) {
+            window.location.href = 'index.html';
+        }
     }
 });
